e1926467c239f4fd9f48540338f894cafebe4adb
Tan Nian Wei <tannianwei@aggienetwork.com>
2020-11-21 12:00:05 +0800

    feat: added note on pre-commit in postgen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,8 +7,15 @@ project_name = "{{ cookiecutter.project_name }}"
 project_path = os.path.abspath(os.path.join("..", project_name))
 
 print("New project directory created at %s" % project_path)
+print()
 
 ### Print helpful statements ---------------------------------------
+
+print(
+"""A default .pre-commit-config.yaml is included in the project dir.
+To use it, install `pre-commit` and run `pre-commit install` in
+the project dir. Refer to https://pre-commit.com/ for more info.""")
+print()
 
 ### Exit -----------------------------------------------------------
 
