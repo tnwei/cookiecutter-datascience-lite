5978cb1e8fc1e3320df9f9eeb33934269652ab08
Tan Nian Wei <tannianwei@aggienetwork.com>
2022-03-27 11:32:44 +0800

    fix: fixed and simplified default logger

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils.py
@@ -1,10 +1,14 @@
 import logging
 import logging.config
+from pathlib import Path
 
-def get_logger(name, level=logging.INFO, conf_file_loc="../conf/logging.yml"):
+wd = Path(__file__).parent.resolve()
+
+
+def get_logger(name, level=logging.INFO, conf_file_loc=wd / "../conf/logging.yml"):
     """
     Python logger object with reasonable defaults.
-    Uses hard-coded configuration in absence of PyYAML or the configuration file.
+    Writes to `logs/NAME.log`
 
     Usage
     -----
@@ -24,48 +28,40 @@ def get_logger(name, level=logging.INFO, conf_file_loc="../conf/logging.yml"):
     if name is None:
         name = "root"
 
-    # Check if can load yaml or if the file exists
-    try:
-        import yaml
-
-        with open(conf_file_loc, "r") as f:
-            config = yaml.safe_load(f.read())
-
-    except:
-        # If not possible, use hard-coded configuration
-        config = {
-            "formatters": {
-                "detailed": {
-                    "datefmt": "%Y-%m-%d %H:%M:%S+%z",
-                    "format": "%(asctime)s | %(name)s | (%(levelname)s) | %(message)s",
-                },
-                "simple": {
-                    "datefmt": "%Y-%m-%d %H:%M:%S+%z",
-                    "format": "(%(levelname)s): %(message)s",
-                },
+    # If not possible, use hard-coded configuration
+    config = {
+        "formatters": {
+            "detailed": {
+                "datefmt": "%Y-%m-%d %H:%M:%S+%z",
+                "format": "%(asctime)s | %(name)s | (%(levelname)s) | %(message)s",
             },
-            "handlers": {
-                "console": {
-                    "class": "logging.StreamHandler",
-                    "formatter": "simple",
-                    "level": "DEBUG",
-                    "stream": "ext://sys.stdout",
-                },
-                "file": {
-                    "backupCount": 3,
-                    "class": "logging.handlers.RotatingFileHandler",
-                    "filename": "../logs/logoutput.log",
-                    "formatter": "detailed",
-                    "maxBytes": 4096,
-                },
+            "simple": {
+                "datefmt": "%Y-%m-%d %H:%M:%S+%z",
+                "format": "(%(levelname)s): %(message)s",
             },
-            "root": {"handlers": ["console", "file"], "level": "DEBUG"},
-            "version": 1,
-        }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "simple",
+                "level": "DEBUG",
+                "stream": "ext://sys.stdout",
+            },
+            "file": {
+                "backupCount": 3,
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": wd / f"../logs/{name}.log",
+                "formatter": "detailed",
+                "maxBytes": 10485760,  # 10 MB
+                "level": "DEBUG",
+            },
+        },
+        "root": {"handlers": ["console", "file"], "level": "DEBUG"},
+        "version": 1,
+    }
 
     logging.config.dictConfig(config)
     logger = logging.getLogger(name)
-    logger.setLevel(level) 
+    logger.setLevel(level)
 
     return logger
-
